7f1a912947afd762b7a6f4ffa05558a87f971a75
Yann Kerherve <yann@cyberion.net>
2009-09-30 12:08:16 -0700

    More generic doc

--- lib/ControlFreak.pm
+++ lib/ControlFreak.pm
@@ -21,10 +21,40 @@ ControlFreak - a process supervisor
 
 =head1 DESCRIPTION
 
-ControlFreak is a process supervisor. It consists of a set of pure
+ControlFreak is a process supervisor. It consists in a set of pure
 Perl classes, a controlling process usally running in the background and 
 a command line tool to talk to it.
 
+It is not a replacement for the init process, init.d etc... The initial goal
+of ControlFreak is to simplify the management of all the processes required
+to run a modern web application. An average web app would use:
+
+=over 4
+
+=item * Memcached
+
+=item * A web reverse proxy or balancer, like Perlbal
+
+=item * Multiple kind of workers
+
+=item * A web server or an application server (apache, fastcgi, ...)
+
+=back
+
+More complex environments add a lot of additional services.
+
+In production you want to tightly control those, making sure there are up
+and running nomimally. You also want an easy way to do code pushes and soft
+roll releases.
+
+In development you usually want to duplicate the production stack which is
+a lot of services that you have to tweak and sometimes restart repeatedly, and
+be able to slightly tweak based on the developer, the code branch etc...
+
+In test, you want a few of these services, and you want to programatically
+control them (making sure there are up or down)
+
+Pid management is always a nightmare when you want to cover all these needs.
 
 
 =head1 AUTHOR
