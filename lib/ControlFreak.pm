62d9a2de93d934c88b2ee8ddc2cb13b515f19434
Yann Kerherve <yann@cyberion.net>
2009-09-30 10:31:08 -0700

    initial import

---
+++ lib/ControlFreak.pm
@@ -0,0 +1,41 @@
+package ControlFreak;
+
+use strict;
+use 5.008_001;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=for stopwords
+
+=head1 NAME
+
+ControlFreak - a process supervisor
+
+=head1 SYNOPSIS
+
+  use ControlFreak;
+
+=head1 DESCRIPTION
+
+ControlFreak is a process supervisor. It consists of a set of pure
+Perl classes, a controlling process usally running in the background and 
+a command line tool to talk to it.
+
+
+
+=head1 AUTHOR
+
+Yann Kerherve E<lt>yannk@cpan.orgE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
