117202b95931fbea7655fd79ae17e5034067cd6a
Yann Kerherve <yann@cyberion.net>
2009-10-02 15:19:34 -0700

    Add the type of watcher to the log
    It's additional flexibility given to the user

--- lib/ControlFreak/Logger.pm
+++ lib/ControlFreak/Logger.pm
@@ -5,15 +5,14 @@ use warnings;
 use Log::Log4perl;
 use Log::Log4perl::Level;
 
-use Object::Tiny qw{
-};
+use Object::Tiny qw{ ctrl };
 
 sub svc_watcher {
     my $logger = shift;
     ## type is 'out' our 'err'
     my ($type, $svcname) = @_;
 
-    my $l4p = Log::Log4perl->get_logger("service.$svcname");
+    my $l4p = Log::Log4perl->get_logger("service.$svcname.$type");
     ## configurable?
     my $loglevel = $type eq 'err' ? $ERROR : $INFO;
     my $watcher_cb = sub {
