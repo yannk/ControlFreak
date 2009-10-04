22ce9a8a22f43b51698d365a276582a9b1392f21
Yann Kerherve <yann@cyberion.net>
2009-10-03 22:46:12 -0700

    Ahaha, the console is now live

--- lib/ControlFreak/Console.pm
+++ lib/ControlFreak/Console.pm
@@ -2,10 +2,13 @@ package ControlFreak::Console;
 use strict;
 use warnings;
 
-use AnyEvent::Socket;
 use Log::Log4perl qw/:easy/;
 Log::Log4perl->easy_init($DEBUG);
 use Carp;
+use AnyEvent::Socket();
+use AnyEvent::Handle();
+
+our $CRLF = "\015\12";
 
 use Object::Tiny qw{
     host
@@ -69,7 +72,7 @@ sub start {
     my $accept_cb = sub {
         my ($fh, $host, $port) = @_;
         INFO "new connection to admin from $host:$port";
-#        $ctrl->accept_admin_connection($fh);
+        $console->accept_connection($fh, $host, $port);
     };
 
     my $prepare_cb = sub {
@@ -82,9 +85,60 @@ sub start {
     $console->{started} = 1;
     my $host = $console->host;
     my $service = $console->service;
-    my $guard = tcp_server $host, $service, $accept_cb, $prepare_cb;
-    return $guard;
+    my $guard = AnyEvent::Socket::tcp_server
+                $host, $service, $accept_cb, $prepare_cb;
+    $console->{guard} = $guard;
+    return 1;
 }
+
+sub accept_connection {
+   my $console = shift;
+   my ($fh, $host, $service) = @_;
+
+    my $hdl; $hdl = AnyEvent::Handle->new(
+        fh       => $fh,
+        on_eof   => sub {
+            INFO "client connection: eof";
+            $hdl->destroy;
+        },
+        on_error => sub {
+            ERROR "Client connection error: $!";
+        },
+    );
+    $console->{handles}{$hdl} = $hdl;
+
+    my $get_admin_cmd; $get_admin_cmd = sub {
+        my ($h, $line) = @_;
+        if (lc $line eq 'exit') {
+            INFO "exiting";
+            $h->on_drain(sub {
+                delete $console->{handles}{$h};
+                $h->destroy;
+            });
+            return 1;
+        }
+
+        ControlFreak::Command->process(
+            cmd => $line,
+            ctrl => $console->{ctrl},
+            err_cb => sub {
+                my $error = shift;
+                $h->push_write("ERROR: $error$CRLF");
+            },
+            ok_cb => sub {
+                $h->push_write("OK$CRLF");
+            },
+            has_priv => $console->full,
+        );
+
+        ## continue reading
+        $h->push_read( line => $get_admin_cmd );
+    };
+
+    $hdl->push_read( line => $get_admin_cmd );
+    $hdl->push_write("cfkcon>$CRLF");
+}
+
 =head1 AUTHOR
 
 Yann Kerherve <yannk@cpan.org>
